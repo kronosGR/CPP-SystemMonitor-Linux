b6634b2276d628d0c556ac97c873422921e2c5a0
KronosGR <geo.elgeo@gmail.com>
2021-06-04 13:46:29 +0200

    processor finished. Needs debug for correct nums

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,23 @@
-#include "processor.h"
+#include <vector>
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+#include "processor.h"
+#include "linux_parser.h"
+
+// Return the aggregate CPU utilization
+float Processor::Utilization() { 
+
+  std::vector<std::string> cpu = LinuxParser::CpuUtilization();
+
+  float totalIdle = std::stol(cpu[3]) + std::stol(cpu[4]);
+  float totalUsed = std::stol(cpu[0]) + std::stol(cpu[1]) + std::stol(cpu[2]) +  
+        std::stol(cpu[5]) + std::stol(cpu[6]) + std::stol(cpu[7]) +
+        std::stol(cpu[8]) + std::stol(cpu[9]);
+
+  float difInUse = ((totalUsed - totalUseT) - (totalIdle - totalIdleT)) / 
+    (totalUsed - totalUseT);
+
+  totalIdleT = totalIdle;
+  totalUseT = totalUsed;
+
+  return difInUse;
+}
--- src/system.cpp
+++ src/system.cpp
@@ -13,26 +13,59 @@ using std::size_t;
 using std::string;
 using std::vector;
 
-// TODO: Return the system's CPU
-Processor& System::Cpu() { return cpu_; }
+// Return the system's CPU
+Processor& System::Cpu() { 
+  return cpu_;
+}
 
-// TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+bool Compare(Process &a, Process &b){
+  return b.CpuUtilization() < a.CpuUtilization();
+}
 
-// TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
+// Return a container composed of the system's processes
+vector<Process>& System::Processes() { 
+  vector<int> pids = LinuxParser::Pids();
+  vector<Process> procs;
 
-// TODO: Return the system's memory utilization
-float System::MemoryUtilization() { return 0.0; }
+  for(int i: pids){
+    Process p(i);
+    procs.push_back(p);
+  }
 
-// TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+  // sort by cpu usage
+  sort(procs.begin(), procs.end(), Compare);
 
-// TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() { return 0; }
+  processes_ = procs;
 
-// TODO: Return the total number of processes on the system
-int System::TotalProcesses() { return 0; }
+  return processes_; 
+}
 
-// TODO: Return the number of seconds since the system started running
-long int System::UpTime() { return 0; }
+// Return the system's kernel identifier (string)
+std::string System::Kernel() { 
+  return LinuxParser::Kernel();
+}
+
+// Return the system's memory utilization
+float System::MemoryUtilization() { 
+  return LinuxParser::MemoryUtilization(); 
+}
+
+// Return the operating system name
+std::string System::OperatingSystem() { 
+  return LinuxParser::OperatingSystem(); 
+}
+
+// Return the number of processes actively running on the system
+int System::RunningProcesses() { 
+  return LinuxParser::RunningProcesses();
+}
+
+// Return the total number of processes on the system
+int System::TotalProcesses() { 
+  return LinuxParser::TotalProcesses();
+}
+
+// Return the number of seconds since the system started running
+long int System::UpTime() { 
+  return LinuxParser::UpTime();
+}
